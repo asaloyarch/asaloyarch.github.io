f0205f43972b84592a068ebd098bbc5d12ab8353
asaloyarch <amanbayeva95@gmail.com>
2019-06-05 22:30:58 +0300

    Updates

---
+++ precache-manifest.2de9341db4a33059898328a56228747d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "107c2d64ebdc5dd337c53679a8c685e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "003fa7ab0f9a9d5e87f6",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "354cff1ef9901d20907a",
+    "url": "/static/css/main.b2533988.chunk.css"
+  },
+  {
+    "revision": "003fa7ab0f9a9d5e87f6",
+    "url": "/static/js/2.2d5db628.chunk.js"
+  },
+  {
+    "revision": "354cff1ef9901d20907a",
+    "url": "/static/js/main.052fe04a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
